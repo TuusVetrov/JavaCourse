17687528288123d1e1d155cf8a0f4e47004a8a61
Никита Клёмин <huk.walker@gmail.com>
2023-09-10 20:47:49 +0300

    4.18 - 4.20

---
+++ Exercise4.18/src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("/persons")
+    public Iterable<Person> getPersons() {
+        return personService.getPersons();
+    }
+
+    @GetMapping("/persons/{id}")
+    public Optional<Person> findPersonById(@PathVariable int id) {
+        return personService.findPersonById(id);
+    }
+
+    @PostMapping("/persons")
+    public Person addPerson(@RequestBody Person person) {
+        return personService.addPerson(person);
+    }
+
+    @PutMapping("/persons/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
+        return personService.updatePerson(id, person);
+    }
+
+    @DeleteMapping("/persons/{id}")
+    public void deletePerson(@PathVariable int id) {
+        personService.deletePerson(id);
+    }
+
+    @DeleteMapping("/persons/{id}/messages/{messageId}")
+    public ResponseEntity<Void> deleteMessage(@PathVariable int id, @PathVariable int messageId) {
+        return messageService.deleteMessageById(id, messageId);
+    }
+
+    @GetMapping("/persons/{id}/messages")
+    public ResponseEntity<List<Message>> getMessagesByUserId(@PathVariable int id) {
+        return messageService.getMessagesByUserId(id);
+    }
+
+    @PostMapping("/persons/{id}/messages")
+    public ResponseEntity<Person> addMessage(@PathVariable int id, @RequestBody Message message) {
+        return messageService.addMessageToPerson(id, message);
+    }
+}
---
+++ Exercise4.18/src/main/java/com/example/demo/PersonService.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonService {
+    @Autowired
+    PersonRepository repository;
+
+    public Iterable<Person> getPersons() {
+        return repository.findAll();
+    }
+
+    public Optional<Person> findPersonById(int id) {
+        return repository.findById(id);
+    }
+
+    public Person addPerson(Person person) {
+        repository.save(person);
+        return person;
+    }
+
+    public ResponseEntity<Person> updatePerson(int id, Person person) {
+        Optional<Person> existingPerson = repository.findById(id);
+
+        if (existingPerson.isEmpty()) {
+            return new ResponseEntity<>(this.repository.save(person), HttpStatus.CREATED);
+        }
+
+        Person updatedPerson = existingPerson.get();
+        updatedPerson.setFirstname(person.getFirstname());
+        updatedPerson.setSurname(person.getSurname());
+        updatedPerson.setLastname(person.getLastname());
+        updatedPerson.setLastname(person.getLastname());
+
+        return new ResponseEntity<>(this.repository.save(updatedPerson), HttpStatus.OK);
+    }
+
+    public void deletePerson(int id) {
+        repository.deleteById(id);
+    }
+}
